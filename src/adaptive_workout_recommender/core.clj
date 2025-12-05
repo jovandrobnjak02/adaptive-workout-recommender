55c181c4882e3150bc679bd6abeee26e5be30f07
jovandrobnjak02 <drobnjakj02@gmail.com>
2025-12-05 21:38:32 +0100

    Initial project setup

---
+++ src/adaptive_workout_recommender/core.clj
@@ -0,0 +1,6 @@
+(ns adaptive-workout-recommender.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/adaptive_workout_recommender/core_test.clj
@@ -0,0 +1,7 @@
+(ns adaptive-workout-recommender.core-test
+  (:require [clojure.test :refer :all]
+            [adaptive-workout-recommender.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
