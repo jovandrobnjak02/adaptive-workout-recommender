ce1ec512fcaa97d4976ab3fa9a61f1ab5573c7eb
jovandrobnjak02 <drobnjakj02@gmail.com>
2025-12-08 17:31:41 +0100

    Add 5 more function that fit the project

--- src/adaptive_workout_recommender/core.clj
+++ src/adaptive_workout_recommender/core.clj
@@ -37,3 +37,44 @@
   (let [goal-ex (filter-by-goal exercises goal)
         muscle-ex (filter-by-muscle-group goal-ex target-muscles)]
     (select-random-exercises muscle-ex n)))
+
+(defn calculate-muscle-imbalance
+  "Given a frequency map {:chest 3 :back 1 ...}, returns a sorted list
+   of muscle groups from least to most trained."
+  [freq-map]
+  (sort-by val freq-map))
+
+(defn recommend-muscle-focus
+  "Return the muscle group with the lowest training frequency."
+  [freq-map]
+  (first (first (calculate-muscle-imbalance freq-map))))
+
+(defn filter-by-difficulty
+  "Keep exercises at or below user's difficulty level."
+  [exercises level]
+  (filter (fn [ex]
+            (let [d (:difficulty ex)]
+              (or (= d level)
+                  (= level :advanced)
+                  (and (= level :intermediate)
+                       (= d :beginner)))))
+          exercises))
+
+(defn generate-balanced-day
+  "Generate a workout day that focuses on the least-trained muscle group."
+  [exercises goal previous-days n]
+  (let [freq (muscle-frequency previous-days)
+        target (recommend-muscle-focus freq)
+        goal-ex (filter-by-goal exercises goal)
+        muscle-ex (filter-by-muscle-group goal-ex #{target})]
+    (select-random-exercises muscle-ex n)))
+
+(defn generate-week-plan
+  "Generate a full workout plan for N days."
+  [exercises goal n]
+  (loop [day 1
+         plan []]
+    (if (> day n)
+      plan
+      (let [workout (generate-workout-day exercises goal #{:chest :back :legs :shoulders} 5)]
+        (recur (inc day) (conj plan workout))))))
