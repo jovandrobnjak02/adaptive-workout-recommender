9055d6277776cdf62f771e3b9a96d752072d9a93
jovandrobnjak02 <drobnjakj02@gmail.com>
2026-01-04 19:33:22 +0100

    Minor fix

--- src/adaptive_workout_recommender/service/generate.clj
+++ src/adaptive_workout_recommender/service/generate.clj
@@ -8,42 +8,51 @@
     [adaptive-workout-recommender.persistence.model-weights :as mw-repo]
     [adaptive-workout-recommender.persistence.db :as db]))
 
+(defn- db-exercise->domain
+  "Convert a DB exercise row into the domain shape used by the generator."
+  [e]
+  {:exercise/id (:id e)
+   :name (:name e)
+   :main-muscle (keyword (:main_muscle e))
+   :secondary-muscle (some-> (:secondary_muscle e) keyword)
+   :difficulty (keyword (:difficulty e))})
+
 (defn generate-and-save-workout!
-  "Loads required inputs, generates workout, saves workout+prescription, returns workout map incl workout-id."
+  "Loads required inputs, generates workout, persists it (workouts + workout_exercises + readiness),
+   returns workout map including :workout-id."
   [ds {:keys [user-id profile readiness]}]
   (jdbc/with-transaction [tx ds]
-                         (let [exercises (ex-repo/list-exercises tx)
-                               last-idx (w-repo/last-sequence-index tx user-id)
-                               last-loads (h-repo/last-load-by-exercise tx user-id)
-                               days-since (h-repo/days-since-last-by-exercise tx user-id)
-                               weights (mw-repo/get-weights-by-exercise tx user-id)
+                         (let [exercise-rows (ex-repo/list-exercises tx)
+                               exercises     (map db-exercise->domain exercise-rows)
 
-                               workout (gen/generate-workout
-                                         {:profile profile
-                                          :history {:last-sequence-index last-idx}
-                                          :readiness readiness
-                                          :exercises (map (fn [e]
-                                                            {:exercise/id (:id e)
-                                                             :name (:name e)
-                                                             :main-muscle (keyword (:main_muscle e))
-                                                             :secondary-muscle (some-> (:secondary_muscle e) keyword)
-                                                             :difficulty (keyword (:difficulty e))})
-                                                          exercises)
-                                          :last-load-by-exercise last-loads
-                                          :weights-by-exercise weights
-                                          :days-since-last-by-exercise days-since})
+                               last-idx    (w-repo/last-sequence-index tx user-id)
+                               last-loads  (h-repo/last-load-by-exercise tx user-id)
+                               days-since  (h-repo/days-since-last-by-exercise tx user-id)
+                               weights     (mw-repo/get-weights-by-exercise tx user-id)
 
-                               workout-id (w-repo/create-workout!
-                                            tx {:user-id user-id
-                                                :template (:template workout)
-                                                :sequence-index (:sequence-index workout)})]
+                               workout     (gen/generate-workout
+                                             {:profile profile
+                                              :history {:last-sequence-index last-idx}
+                                              :readiness readiness
+                                              :exercises exercises
+                                              :last-load-by-exercise last-loads
+                                              :weights-by-exercise weights
+                                              :days-since-last-by-exercise days-since})
+
+                               workout-id  (w-repo/create-workout!
+                                             tx {:user-id user-id
+                                                 :template (:template workout)
+                                                 :sequence-index (:sequence-index workout)})]
 
                            (w-repo/insert-workout-exercises! tx workout-id (:exercises workout))
 
-                           ;; save readiness row
                            (db/exec! tx
                                      ["INSERT INTO readiness (workout_id, stress, fatigue, sleep, nutrition)
-                  VALUES (?, ?, ?, ?, ?)"
-                                      workout-id (:stress readiness) (:fatigue readiness) (:sleep readiness) (name (:nutrition readiness))])
+                  VALUES ($1, $2, $3, $4, $5)"
+                                      workout-id
+                                      (:stress readiness)
+                                      (:fatigue readiness)
+                                      (:sleep readiness)
+                                      (name (:nutrition readiness))])
 
                            (assoc workout :workout-id workout-id))))
